bc2d4c66fe72092b85d91ffb156bd2aa9d828561
pedroce2323 <pedro.cebollero@kellogg.northwestern.edu>
2020-02-24 00:59:00 +0000

    assignment 3 update 2

--- app.rb
+++ app.rb
@@ -23,22 +23,42 @@ get "/news" do
 
 
     # # do the heavy lifting, use Global Hub lat/long
-    forecast = ForecastIO.forecast("#{@lat}", "#{@lng}").to_hash
+    @forecast = ForecastIO.forecast("#{@lat}", "#{@lng}").to_hash
     
-    @current_temp = forecast["currently"]["temperature"]
-    @current_summary = forecast["currently"]["summary"]
+    @current_temp = @forecast["currently"]["temperature"]
+    @current_summary = @forecast["currently"]["summary"]
 
 
     i=1
 
     @forecast_array = []
 
-    for days in forecast["daily"]["data"]
+    for days in @forecast["daily"]["data"]
         @forecast_array << "Day #{i}: A high temperature of #{days["temperatureHigh"]} and #{days["summary"]}"
         i = i + 1
     end
 
     puts @forecast_array
+
+    # code for news portion of the website starts here
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=7cd39bd834074372a1d972973b13df52"
+    @news = HTTParty.get(url).parsed_response.to_hash
+    
+    # news headlines
+    article_number=0
+    @news_array = []
+    @link_array = []
+    for articles in @news["articles"]
+        @news_array << @news["articles"][article_number]["title"]
+        @link_array << @news["articles"][article_number]["url"]
+        article_number = article_number + 1
+    end
+
+
+    
+    puts @news_array
+
+    # news is now a Hash you can pretty print (pp) and parse for your output
 
     view "news"
 end
